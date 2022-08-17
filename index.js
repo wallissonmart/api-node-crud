40c5d62fdd99b6c82172092160ba440235de9825
Wallisson Martins <wallissonmartins37@gmail.com>
2022-08-17 09:08:18 -0300

    edit

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const app = require('./server')
 const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta: http://localhost:${PORT}`)
+  console.log(`Servidor rod na porta: http://localhost:${PORT}`)
 })
